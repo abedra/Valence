6184efb6d36c434b4ffa5f0a1ca17e1a445f4424
Aaron Bedra <aaron@aaronbedra.com>
2012-04-06 15:20:00 -0500

    Adding 'if'

--- valence.hs
+++ valence.hs
@@ -167,6 +167,11 @@ eval :: LispVal -> ThrowsError LispVal
 eval val@(String _) = return val
 eval val@(Number _) = return val
 eval val@(Bool _) = return val
+eval (List [Atom "if", pred, conseq, alt]) =
+     do result <- eval pred
+        case result of
+             Bool False -> eval alt
+             otherwise -> eval conseq
 eval (List [Atom "quote", val]) = return val
 eval (List (Atom func : args)) = mapM eval args >>= apply func
 eval badForm = throwError $ BadSpecialForm "Unrecognized special form" badForm
